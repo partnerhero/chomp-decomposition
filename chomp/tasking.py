bff5af81050eb42a4830d74761836d9c0e5a1dbb
Philip I. Thomas <mail@philipithomas.com>
2017-06-22 18:41:56 -0500

    Add custom url base

--- chomp/tasking.py
+++ chomp/tasking.py
@@ -18,7 +18,7 @@ class Tasking():
     REQUEUE_STATE = "chomp-queue"
 
     def __init__(self):
-        self.client = Client(key=config.STAFFJOY_API_KEY, env=config.ENV)
+        self.client = Client(key=config.STAFFJOY_API_KEY, env=config.ENV, url_base="https://staffjoy.partnerhero.com/api/v2/")
         self.default_tz = pytz.timezone(config.DEFAULT_TZ)
 
         # To be defined later
